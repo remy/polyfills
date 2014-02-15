898b546b1e1d94d9a3ce4d2f81bb2359fef05193
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2014-02-16 01:00:51 +0800

    Code clean up, reduction and remove unnecessary Array.indexOf polyfill that incidentally interferes with IE8's fireEvent, there's es5-shim for that if needed

--- classList.js
+++ classList.js
@@ -2,63 +2,40 @@
 
 if (typeof window.Element === "undefined" || "classList" in document.documentElement) return;
 
-// adds indexOf to Array prototype for IE support
-if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function(obj, start) {
-        for (var i = (start || 0), j = this.length; i < j; i++) {
-            if (this[i] === obj) { return i; }
-        }
-        return -1;
-    }
-}
-
 var prototype = Array.prototype,
-    indexOf = prototype.indexOf,
-    slice = prototype.slice,
     push = prototype.push,
     splice = prototype.splice,
     join = prototype.join;
 
-function DOMTokenList(el) {  
-  this._element = el;
-  if (el.className != this._classCache) {
-    this._classCache = el.className;
-
-    if (!this._classCache) return;
-    
-      // The className needs to be trimmed and split on whitespace
-      // to retrieve a list of classes.
-      var classes = this._classCache.replace(/^\s+|\s+$/g,'').split(/\s+/),
-        i;
-    for (i = 0; i < classes.length; i++) {
-      push.call(this, classes[i]);
-    }
+function DOMTokenList(el) {
+  this.el = el;
+  // The className needs to be trimmed and split on whitespace
+  // to retrieve a list of classes.
+  var classes = el.className.replace(/^\s+|\s+$/g,'').split(/\s+/);
+  for (var i = 0; i < classes.length; i++) {
+    push.call(this, classes[i]);
   }
 };
-
-function setToClassName(el, classes) {
-  el.className = classes.join(' ');
-}
 
 DOMTokenList.prototype = {
   add: function(token) {
     if(this.contains(token)) return;
     push.call(this, token);
-    setToClassName(this._element, slice.call(this, 0));
+    this.el.className = this.toString();
   },
   contains: function(token) {
-    return indexOf.call(this, token) !== -1;
+    return this.el.className.indexOf(token) != -1;
   },
   item: function(index) {
     return this[index] || null;
   },
   remove: function(token) {
-    var i = indexOf.call(this, token);
-     if (i === -1) {
-       return;
-     }
+    if (!this.contains(token)) return;
+    for (var i = 0; i < this.length; i++) {
+      if (this[i] == token) break;
+    }
     splice.call(this, i, 1);
-    setToClassName(this._element, slice.call(this, 0));
+    this.el.className = this.toString();
   },
   toString: function() {
     return join.call(this, ' ');
@@ -77,17 +54,17 @@ DOMTokenList.prototype = {
 window.DOMTokenList = DOMTokenList;
 
 function defineElementGetter (obj, prop, getter) {
-	if (Object.defineProperty) {
-		Object.defineProperty(obj, prop,{
-			get : getter
-		})
-	} else {					
-		obj.__defineGetter__(prop, getter);
-	}
+    if (Object.defineProperty) {
+        Object.defineProperty(obj, prop,{
+            get : getter
+        });
+    } else {
+        obj.__defineGetter__(prop, getter);
+    }
 }
 
 defineElementGetter(Element.prototype, 'classList', function () {
-  return new DOMTokenList(this);			
+  return new DOMTokenList(this);
 });
 
 })();
